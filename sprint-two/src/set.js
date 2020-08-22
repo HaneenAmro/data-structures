390f821d6795747d02809921db77053d9e54a274
Haneen Amro <heno.amro@gmail.com>
2020-08-22 11:28:43 -0700

    'Implemented Set function and the methods of set'

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // add key value pair of item to _storage object;
+  //Time Complexity : constant;
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  // check if object has item and return true if so, if not return false
+  // Time Complexity: linear??
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  // delete property
+  //Time Comlxity : constant
+  delete this._storage[item];
 };
 
 /*
